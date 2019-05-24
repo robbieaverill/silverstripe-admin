ac5d723438e7d4236e16ed5e5d0381b6eba8b4fc
Robbie Averill <robbie@silverstripe.com>
2019-05-24 14:00:48 +1200

    Add entwine loader

--- client/src/legacy/ConfirmedPasswordField.js
+++ client/src/legacy/ConfirmedPasswordField.js
@@ -1,17 +1,46 @@
-import $ from 'jquery';
+/* global window */
+import jQuery from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { schemaMerge } from 'lib/schemaFieldValues';
+import { loadComponent } from 'lib/Injector';
 
-// TODO Enable once https://github.com/webpack/extract-text-webpack-plugin/issues/179 is resolved. Included in bundle.scss for now.
-// require('../styles/legacy/ConfirmedPasswordField.scss');
+jQuery.entwine('ss', ($) => {
+  $('.js-injector-boot .confirmedpassword').entwine({
+    onmatch() {
+      this._super();
 
-$(document).on('click', '.confirmedpassword .showOnClick a', function () {
-  var $container = $('.showOnClickContainer', $(this).parent());
+      const cmsContent = this.closest('.cms-content').attr('id');
+      const context = (cmsContent)
+        ? { context: cmsContent }
+        : {};
+      const ConfirmedPasswordField = loadComponent('ConfirmedPasswordField', context);
 
-  $container.toggle('fast', function() {
-    $container
-      .toggleClass('d-none')
-      .find('input[type="hidden"]')
-      .val($container.hasClass('d-none') ? 0 : 1);
+      ReactDOM.render(
+        <ConfirmedPasswordField
+          {...this.getAttributes()}
+        />,
+        this[0]
+      );
+    },
+
+    onunmatch() {
+      this._super();
+      // solves errors given by ReactDOM "no matched root found" error.
+      const container = this[0];
+      if (container) {
+        ReactDOM.unmountComponentAtNode(container);
+      }
+    },
+
+    /**
+     * @returns {object}
+     */
+    getAttributes() {
+      const state = this.data('state');
+      const schema = this.data('schema');
+      return schemaMerge(schema, state);
+    },
   });
-
-  return false;
 });
+
